bda512b613dcbb62b86b023995ee6a84c71f2b85
Dan <email@danielupshaw.com>
2023-09-23 05:29:25 -0400

    View Package structure

---
+++ view/Lipupini/SingleFile/Close.php
@@ -0,0 +1,6 @@
+</div>
+<footer class="app-bar">
+	<div class="previous"><button title="Previous">←</button></div>
+	<div class="index"><button title="Index"></button></div>
+	<div class="next"><button title="Next" disabled="">→</button></div>
+</footer>
