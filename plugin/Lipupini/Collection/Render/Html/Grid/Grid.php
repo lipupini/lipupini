5c36d6d382dde7b8208e1bff63414fb596743175
Dan <d@dup.bz>
2023-09-28 19:56:56 -0400

    Responsive CSS

--- plugin/Lipupini/Collection/Render/Html/Grid/Grid.php
+++ plugin/Lipupini/Collection/Render/Html/Grid/Grid.php
@@ -5,7 +5,7 @@
 	<div class="next"><button title="Next" disabled="">→</button></div>
 	<div class="about"><button title="More information about this software">?</button></div>
 </header>-->
-<div id="media-container" class="grid square" style="grid-template-columns: repeat(6, minmax(0px, 1fr)); columns: unset;"></div>
+<div id="media-container" class="grid square"></div>
 <!--<footer class="app-bar">
 	<div class="previous"><button title="Previous">←</button></div>
 	<div class="index"><button title="Index"></button></div>
