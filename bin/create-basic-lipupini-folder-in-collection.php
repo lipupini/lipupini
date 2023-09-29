99f941d8d0f38a7a7d72daf7b8a4cb0981757dce
Dan <d@dup.bz>
2023-09-28 22:22:32 -0400

    Add a script to init a `.lipupini` folder for a new collection folder

---
+++ bin/create-basic-lipupini-folder-in-collection.php
@@ -0,0 +1,68 @@
+#!/usr/bin/env php
+<?php
+
+use System\Lipupini;
+use Plugin\Lipupini\Encryption;
+
+require(__DIR__ . '/../package/vendor/autoload.php');
+
+// Expects a username as the only argument
+$collectionFolder = $argv[1];
+
+Lipupini::validateCollectionFolderName($collectionFolder);
+
+$collectionPath = DIR_COLLECTION . '/' . $collectionFolder;
+$lipupiniPath = $collectionPath . '/.lipupini';
+
+// Create the `.lipupini` subfolder if needed
+if (!is_dir($collectionPath)) {
+	echo 'Creating `.lipupini` folder...' . "\n";
+	mkdir($lipupiniFolder, 0755, true);
+}
+
+// Generate RSA keypair if needed
+if (
+	!file_exists($collectionPath . '/.rsakey.public') ||
+	!file_exists($collectionPath . '/.rsakey.private')
+) {
+	echo 'Did not find RSA keypair. Creating...' . "\n";
+	$encryption = new Encryption(2048);
+	$encryption->generateAndSave($collectionPath . '/.rsakey');
+}
+
+// Generate a basic `.files.json` if needed
+if (
+	!file_exists($lipupiniPath . '/.files.json')
+) {
+	echo 'Creating `.files.json`...' . "\n";
+
+	$dir = new \DirectoryIterator($collectionPath);
+	$files = [];
+	foreach ($dir as $fileinfo) {
+		if ($fileinfo->isDot()) {
+			continue;
+		}
+
+		// Skip hidden files
+		if ($fileinfo->getFilename()[0] === '.') {
+			continue;
+		}
+
+		$files[] = [
+			'filename' => $fileinfo->getFilename(),
+			'caption' => '',
+			'date' => (new DateTime)->format(DateTime::ATOM),
+		];
+	}
+
+	file_put_contents($lipupiniPath . '/.files.json', json_encode($files, JSON_PRETTY_PRINT));
+}
+
+// Reminder to add an avatar PNG
+if (
+	!file_exists($lipupiniPath . '/.avatar.png')
+) {
+	echo 'Be sure to add an avatar .png at `' . $lipupiniPath . '/.avatar.png`!' . "\n";
+}
+
+echo 'Done.' . "\n";
