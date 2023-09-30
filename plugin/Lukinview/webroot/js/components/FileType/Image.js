d6082c9556f392c79d7a6dfe54ca3e27e465ada6
Dan <d@dup.bz>
2023-09-30 03:04:07 -0400

    Documents now have a "view page"

--- plugin/Lukinview/webroot/js/components/FileType/Image.js
+++ plugin/Lukinview/webroot/js/components/FileType/Image.js
@@ -6,7 +6,7 @@ const Image = ({collection, filename, data, background = true}) => {
 	let image = img({src: background ? '/img/1x1.png' : `/c/file/${collection}/large/${filename}`, alt: data.caption ?? filename, title: data.caption ?? filename});
 	let anchorAttrs = background ? {href:`/@${collection}/${filename}.html`} : {href:`/c/file/${collection}/large/${filename}`, target: '_blank'}
 
-	return div(
+	return div({class: 'image'},
 		a(anchorAttrs,
 			background ? div({style: 'background-image:url(' + `/c/file/${collection}/small/${filename}` + ')'},
 				image,
