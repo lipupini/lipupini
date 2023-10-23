3cb97f168ad93e41e0bff31465d2778d117d4d31
Dan <email@danielupshaw.com>
2023-10-23 15:14:27 -0400

    Remove debug function

--- module/Lipupini/ActivityPub/Request.php
+++ module/Lipupini/ActivityPub/Request.php
@@ -26,50 +26,6 @@ class Request extends Http {
 			return;
 		}
 
-		// Follow request debug
-		// Switch to true to enable
-		if (false && $_GET['ap'] === 'follow-debug') {
-			$remote = 'dup@pix.dup.bz';
-
-			$remoteActor = RemoteActor::fromHandle(
-				handle: $remote,
-				cacheDir: $this->system->dirStorage . '/cache/ap'
-			);
-
-			$sendToInbox = $remoteActor->getInboxUrl();
-
-			if (!filter_var($sendToInbox, FILTER_VALIDATE_URL)) {
-				throw new Exception('Could not determine inbox URL', 400);
-			}
-
-			// Create the JSON payload for the Follow activity (adjust as needed)
-			$followActivity = [
-				'@context' => 'https://www.w3.org/ns/activitystreams',
-				'id' => $this->system->baseUri . '@' . $this->collectionFolderName . '?ap=profile#follow/' . md5(rand(0, 1000000) . microtime(true)),
-				'type' => 'Follow',
-				'actor' => $this->system->baseUri . '@' . $this->collectionFolderName . '?ap=profile',
-				'object' => $remoteActor->getId(),
-			];
-
-			$activityJson = json_encode($followActivity, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES);
-
-			$test = Outgoing\Http::sendSigned(
-				keyId: $this->system->baseUri . '@' . $this->collectionFolderName . '?ap=profile#main-key',
-				privateKeyPem: file_get_contents($this->system->dirCollection . '/' . $this->collectionFolderName . '/.lipupini/.rsakey.private'),
-				inboxUrl: $remoteActor->getInboxUrl(),
-				body: $activityJson,
-				headers: [
-					'Content-type' => $this::$mimeType,
-					'Accept' => $this::$mimeType,
-					'User-agent' => $this->system->userAgent,
-				]
-			);
-
-			var_dump($test);
-
-			exit('test235234234234');
-		}
-
 		$activityPubRequest = ucfirst($_GET['ap']);
 
 		// This will compute to a class in the `./Request` folder e.g. `./Request/Follow.php`;
