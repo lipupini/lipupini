8f2de9cca2cb4800afce7610b6b880576fa732b9
Dan <email@danielupshaw.com>
2024-02-25 08:18:26 -0500

    Move about link to bottom of page in Document view similar to Folder view

--- module/Lukinview/Html/Collection/Document.php
+++ module/Lukinview/Html/Collection/Document.php
@@ -7,11 +7,9 @@ require(__DIR__ . '/../Core/Open.php') ?>
 
 <main class="media-item">
 <header class="app-bar">
-	<div></div>
 	<div class="previous pagination"></div>
 	<div class="index pagination"><a href="/<?php echo htmlentities($this->parentPath) ?>" class="button" title="<?php echo $this->parentPath ? htmlentities($this->parentPath) : A::z('Homepage') ?>"><img src="/img/arrow-up-bold.svg" alt="<?php echo $this->parentPath ? htmlentities($this->parentPath) : A::z('Homepage') ?>"></a></div>
 	<div class="next pagination"></div>
-	<div class="about"><a href="https://github.com/lipupini/lipupini" target="_blank" rel="noopener noreferrer" class="button" title="<?php echo A::z('More information about this software') ?>">?</a></div>
 </header>
 <div id="media-item"></div>
 <script>let baseUri = '<?php echo htmlentities($this->system->staticMediaBaseUri) ?>';let collection = '<?php echo htmlentities($this->system->requests[Collection\Request::class]->folderName) ?>';let filename = '<?php echo htmlentities(rawurldecode($this->collectionFileName)) ?>';let fileData = <?php echo json_encode($this->fileData, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES) ?>;let fileTypes=<?php echo json_encode($this->fileTypes, JSON_THROW_ON_ERROR | JSON_UNESCAPED_SLASHES) ?>;</script>
@@ -20,6 +18,11 @@ import van from '/lib/van-1.2.7.min.js'
 import { Document } from '/js/components/Document.js'
 van.add(document.getElementById('media-item'), Document({collection, baseUri, filename, data: fileData}))
 </script>
+<footer>
+	<div class="about">
+		<a href="https://github.com/lipupini/lipupini" target="_blank" rel="noopener noreferrer" class="button" title="<?php echo A::z('More information about this software') ?>">?</a>
+	</div>
+</footer>
 </main>
 
 <?php require(__DIR__ . '/../Core/Close.php') ?>
