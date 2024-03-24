4ada0394a999427e135e3fa471a8248f26348697
Dan <email@danielupshaw.com>
2024-03-23 20:55:48 -0400

    Update comment in system config

--- config/system.php
+++ config/system.php
@@ -15,8 +15,8 @@ return new Module\Lipupini\State(
 	frontendModule: 'Lukinview',
 	viewLanguage: 'english',
 	requests: [
-		// Once instantiated by Request\Incoming\Queue `render()`,
-		// each key will hold the module instance itself
+		// Once instantiated by Module\Lipupini\Request\Incoming\Queue `render()`,
+		// each `requests` key here will instead hold the module instance itself
 		Module\Lukinview\HomepageRequest::class => null,
 		Module\Lipupini\WebFinger\Request::class => null,
 		Module\Lipupini\ActivityPub\NodeInfoRequest::class => null,
