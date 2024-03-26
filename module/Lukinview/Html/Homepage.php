4b570c83d063f02ba0191599c29879b35d632abf
Dan <email@danielupshaw.com>
2024-03-26 17:40:29 -0400

    If the collection folder is empty, display a message

--- module/Lukinview/Html/Homepage.php
+++ module/Lukinview/Html/Homepage.php
@@ -1,11 +1,25 @@
-<?php require(__DIR__ . '/Core/Open.php') ?>
+<?php
+
+use Module\Lipupini\L18n\A;
+
+$localCollections = $this->getLocalCollections();
+
+require(__DIR__ . '/Core/Open.php');
+
+if (!empty($localCollections)) : ?>
 
 <ul>
-	<?php foreach ($this->getLocalCollections() as $localCollection) : ?>
+	<?php foreach ($localCollections as $localCollection) : ?>
 
 	<li><a href="/@<?php echo htmlentities($localCollection) ?>"><?php echo htmlentities($localCollection) ?></a></li>
 	<?php endforeach ?>
 
 </ul>
+<?php else : ?>
+
+<div class="full-page-center-text">
+	<a href="https://github.com/lipupini/lipupini/#add-your-collection" target="_blank" rel="noopener noreferrer"><?php echo A::z('Add your collection') ?></kbd></a>
+</div>
+<?php endif ?>
 
 <?php require(__DIR__ . '/Core/Close.php') ?>
--- module/Lukinview/L18n/english.php
+++ module/Lukinview/L18n/english.php
@@ -5,4 +5,5 @@ return [
 	'Next' => 'Next',
 	'Homepage' => 'Homepage',
 	'More information about this software' => 'More information about this software',
+	'Add your collection' => 'Add your collection',
 ];
