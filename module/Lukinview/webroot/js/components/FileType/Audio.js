f6d5af5455ee46339f479080575cd134f8ece11b
Dan <email@danielupshaw.com>
2024-04-02 23:25:19 -0400

    Updates to audio layout in support of cover art option
    Cleanup Instalution artifacts from CSS

--- module/Lukinview/webroot/js/components/FileType/Audio.js
+++ module/Lukinview/webroot/js/components/FileType/Audio.js
@@ -6,14 +6,20 @@ const Audio = ({collection, baseUri, filename, data, mimeType, gridView}) => {
 	collection = encodeURIComponent(collection)
 	let filenameEncoded = filename.split('/').map((uriComponent) => encodeURIComponent(uriComponent)).join('/')
 	let title = data.caption ?? filename.split(/[\\\/]/).pop();
-	return div(
-		{class: 'audio'},
-		div({class: 'thumbnail', style: typeof data.thumbnail !== 'undefined' ?
-			'background-image:url("' + `${baseUri}${collection}/thumbnail/${encodeURIComponent(data.thumbnail)}` + '")' : ''}),
-		div({class: 'caption'}, gridView ? a({href: `/@${collection}/${filenameEncoded}.html`}, title) : title),
-		audio({controls: 'true', preload: 'metadata', title: title, loading: 'lazy'},
+	let thumbnailAttributes = {
+		class: 'thumbnail', style: typeof data.thumbnail !== 'undefined' ?
+			'background-image:url("' + `${baseUri}${collection}/thumbnail/${encodeURIComponent(data.thumbnail)}` + '")' : ''
+	};
+	if (gridView) {
+		thumbnailAttributes.href = `/@${collection}/${filenameEncoded}.html`;
+	}
+	let captionDiv = div({class: 'caption'}, title);
+	return div({class: 'audio', title: title},
+		gridView ? a(thumbnailAttributes, captionDiv) : div(thumbnailAttributes, captionDiv),
+		audio(
+			{controls: 'true', preload: 'metadata', loading: 'lazy'},
 			source({src: `${baseUri}${collection}/audio/${filenameEncoded}`, type: mimeType}),
-		),
+		)
 	)
 }
 
