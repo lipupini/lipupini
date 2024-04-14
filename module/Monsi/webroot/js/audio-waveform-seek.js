af684160802f3b2527ddacf5c1218ad6198b89ca
Dan <email@danielupshaw.com>
2024-04-14 00:28:42 -0400

    Add ability to drag elapsed indicator around audio waveform

--- module/Monsi/webroot/js/audio-waveform-seek.js
+++ module/Monsi/webroot/js/audio-waveform-seek.js
@@ -1,15 +1,33 @@
 document.querySelectorAll('.audio-waveform-seek').forEach(container => {
-	let audio = container.querySelector('audio')
-	let waveform = container.querySelector('.waveform')
-	let elapsed = waveform.querySelector('.elapsed')
+	const audio = container.querySelector('audio')
+	const waveform = container.querySelector('.waveform')
+	const elapsed = waveform.querySelector('.elapsed')
+	const transitionDuration = elapsed.style.transitionDuration
 	audio.addEventListener('play', () => {
 		elapsed.classList.remove('hidden')
 	})
 	audio.addEventListener('timeupdate', () => {
 		elapsed.style.width = ((audio.currentTime / audio.duration) * 100) + '%'
 	})
-	waveform.addEventListener('click', (e) => {
+	let trackingMouseMove = false
+	const moveElapsed = (e) => {
+		if (elapsed.style.transitionDuration !== 'unset') {
+			elapsed.style.transitionDuration = 'unset'
+		}
+		elapsed.style.width = ((e.layerX / waveform.scrollWidth) * 100) + '%'
+	}
+	const applyElapsedChange = () => {
+		waveform.removeEventListener('mousemove', moveElapsed)
+		audio.currentTime = ((parseFloat(elapsed.style.width || 0)) / 100) * (audio.duration || 0)
+		elapsed.style.transitionDuration = transitionDuration
+		trackingMouseMove = false
+	}
+	waveform.addEventListener('mousedown', (e) => {
 		elapsed.classList.remove('hidden')
-		audio.currentTime = (e.offsetX / waveform.scrollWidth) * (audio.duration || 0)
+		elapsed.style.width = ((e.layerX / waveform.scrollWidth) * 100) + '%'
+		waveform.addEventListener('mousemove', moveElapsed)
+		trackingMouseMove = true
 	})
+	waveform.addEventListener('mouseleave', applyElapsedChange)
+	document.addEventListener('mouseup', applyElapsedChange)
 })
