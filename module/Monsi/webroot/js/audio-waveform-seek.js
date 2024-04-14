03142b354683b8df815d2ccf26cf4aad23c9fa69
Dan <email@danielupshaw.com>
2024-04-14 10:13:02 -0400

    Improved mobile support for dragging elapsed on audio waveform

--- module/Monsi/webroot/js/audio-waveform-seek.js
+++ module/Monsi/webroot/js/audio-waveform-seek.js
@@ -4,31 +4,39 @@ document.querySelectorAll('.audio-waveform-seek').forEach(container => {
 	const waveform = container.querySelector('.waveform')
 	const elapsed = waveform.querySelector('.elapsed')
 	const transitionDuration = elapsed.style.transitionDuration
+	let trackingMouseMove = false
+
 	audio.addEventListener('play', () => {
 		elapsed.classList.remove('hidden')
 	})
+
 	audio.addEventListener('timeupdate', () => {
+		if (trackingMouseMove) return
 		elapsed.style.width = ((audio.currentTime / audio.duration) * 100) + '%'
 	})
-	let trackingMouseMove = false
+
 	const moveElapsed = (e) => {
 		if (elapsed.style.transitionDuration !== 'unset') {
 			elapsed.style.transitionDuration = 'unset'
 		}
-		elapsed.style.width = ((e.layerX / waveform.scrollWidth) * 100) + '%'
+		const xVal = isTouchDevice ? e.touches[0].clientX - waveform.getBoundingClientRect().x : e.layerX
+		elapsed.style.width = ((xVal / waveform.scrollWidth) * 100) + '%'
 	}
-	const applyElapsedChange = () => {
+
+	const applyElapsedChange = (e) => {
 		if (!trackingMouseMove) return
 		if (isTouchDevice) {
 			waveform.removeEventListener('touchmove', moveElapsed)
 		} else {
 			waveform.removeEventListener('mousemove', moveElapsed)
 		}
+		trackingMouseMove = false
+		if (e.type === 'touchcancel') return
 		audio.currentTime = ((parseFloat(elapsed.style.width || 0)) / 100) * (audio.duration || 0)
 		elapsed.style.transitionDuration = transitionDuration
-		trackingMouseMove = false
 	}
-	waveform.addEventListener('mousedown', (e) => {
+
+	const mouseDownTouchStart = (e) => {
 		/* See CSS hover state for waveform, on mobile it takes effect on touch */
 		if (isTouchDevice && window.getComputedStyle(waveform).opacity === '0') {
 			return
@@ -36,12 +44,17 @@ document.querySelectorAll('.audio-waveform-seek').forEach(container => {
 		elapsed.classList.remove('hidden')
 		elapsed.style.width = ((e.layerX / waveform.scrollWidth) * 100) + '%'
 		if (isTouchDevice) {
-			waveform.addEventListener('touchmove', moveElapsed)
+			waveform.addEventListener('touchmove', moveElapsed, {passive: true})
 		} else {
 			waveform.addEventListener('mousemove', moveElapsed)
 		}
 		trackingMouseMove = true
-	})
+	}
+
+	waveform.addEventListener('touchstart', mouseDownTouchStart, {passive: true})
+	waveform.addEventListener('mousedown', mouseDownTouchStart)
+	waveform.addEventListener('touchend', applyElapsedChange)
+	waveform.addEventListener('touchcancel', applyElapsedChange)
 	waveform.addEventListener('mouseleave', applyElapsedChange)
 	document.addEventListener('mouseup', applyElapsedChange)
 })
