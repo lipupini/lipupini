29a76f275d156dc1a88802dd806bca21c3b91114
Dan <email@danielupshaw.com>
2024-04-18 14:24:40 -0400

    Updates tests + celebrate PHPStorm 2024.1

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 			use: { ...devices['Desktop Chrome'] },
 		},
 
-		/*{
+		{
 			name: 'firefox',
 			use: { ...devices['Desktop Firefox'] },
 		},
@@ -45,27 +45,27 @@ export default defineConfig({
 		{
 			name: 'webkit',
 			use: { ...devices['Desktop Safari'] },
-		},*/
+		},
 
 		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
+		{
+		  name: 'Mobile Chrome',
+		  use: { ...devices['Pixel 5'] },
+		},
+		{
+		  name: 'Mobile Safari',
+		  use: { ...devices['iPhone 12'] },
+		},
 
 		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		/*{
+		  name: 'Microsoft Edge',
+		  use: { ...devices['Desktop Edge'], channel: 'msedge' },
+		},
+		{
+		  name: 'Google Chrome',
+		  use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+		},*/
 	],
 
 	/* Run your local dev server before starting the tests */
--- test/spec/collection.test.ts
+++ test/spec/collection.test.ts
@@ -1,16 +1,16 @@
-import { test, expect, Page } from '@playwright/test'
+import {test, expect, Page} from '@playwright/test'
 
 const host = 'http://localhost:4000' // Without trailing slash
 
-const glob = require('glob')
-const path = require('path')
-const fs = require('fs')
+import glob = require('glob')
+import path = require('path')
+import fs = require('fs')
 const testAssetsFolder = __dirname + '/../assets'
 const collectionRootFolder = __dirname + '/../../collection'
-const { execSync } = require('child_process')
+import {execSync} from 'child_process'
 
 // If `createNewCollection` is `true`, the `testCollectionName` will have a number appended if it exists
-let testCollectionName = 'testcollection'
+let testCollectionName = 'test-collection'
 
 const createNewCollection = true
 const testCollectionPagination = true
@@ -31,7 +31,7 @@ const testAssetFiles = [
 	'winamp-intro.mp3'
 ]
 
-const askPhp = to => {
+const askPhp = (to: string) => {
 	const command = 'php "' + __dirname + '/../../bin/test-helper.php" ' + to
 	const answer = execSync(command, {stdio: 'pipe'}).toString()
 	try {
@@ -40,6 +40,17 @@ const askPhp = to => {
 		console.log(answer)
 		throw e
 	}
+}
+
+const supportsAvif = async (page: Page) => {
+	return await page.evaluate(async () => {
+		if (typeof createImageBitmap === 'undefined') return false
+		const avifData = 'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAADybWV0YQAAAAAAAAAoaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGxpYmF2aWYAAAAADnBpdG0AAAAAAAEAAAAeaWxvYwAAAABEAAABAAEAAAABAAABGgAAAB0AAAAoaWluZgAAAAAAAQAAABppbmZlAgAAAAABAABhdjAxQ29sb3IAAAAAamlwcnAAAABLaXBjbwAAABRpc3BlAAAAAAAAAAIAAAACAAAAEHBpeGkAAAAAAwgICAAAAAxhdjFDgQ0MAAAAABNjb2xybmNseAACAAIAAYAAAAAXaXBtYQAAAAAAAAABAAEEAQKDBAAAACVtZGF0EgAKCBgANogQEAwgMg8f8D///8WfhwB8+ErK42A='
+		const avifBlob = await fetch(avifData).then((r) => r.blob());
+		return createImageBitmap(avifBlob)
+			.then(() => true)
+			.catch(() => false)
+	})
 }
 
 const hasFfmpeg = askPhp('determineFfmpegSupport')
@@ -63,7 +74,7 @@ if (createNewCollection) {
 	testCollectionFolder.cache = testCollectionFolder.root + '/.lipupini/.cache'
 }
 
-test('click into collection list from homepage and verify all', async ({ page }) => {
+test('click into collection list from homepage and verify all', async ({ page}) => {
 	await page.goto(host + '/')
 
 	await page.locator('a >> nth=0').click()
@@ -84,18 +95,22 @@ test('click into collection list from homepage and verify all', async ({ page })
 
 test.describe.serial('test collection', () => {
 	if (createNewCollection) {
-		test('creates a new test collection', async () => {
+		test('creates a new test collection', async ({page}) => {
 			// Create the folder
 			fs.mkdirSync(testCollectionFolder.root)
+			const supportsAvifResult = await supportsAvif(page)
 			// Populate some test files
-			testAssetFiles.forEach(fileName => {
+			for (const fileName of testAssetFiles) {
+				const extension = fileName.match(/\.(.+)$/)[1]
+				if (extension === 'avif' && !supportsAvifResult) continue
 				fs.copyFileSync(testAssetsFolder + '/' + fileName, testCollectionFolder.root + '/' + fileName)
-			})
+			}
+			await page.waitForTimeout(500) // A little delay to help ensure that the new files are available
 		})
 	}
 
 	if (createNewCollection) {
-		test('add custom assets', async () => {
+		test('add custom assets', async ({page}) => {
 			fs.cpSync(testAssetsFolder + '/image', testCollectionFolder.root + '/.lipupini/image', {recursive: true})
 			if (hasFfmpeg) {
 				// If we have `ffmpeg` then we only need to copy custom assets that aren't generated
@@ -105,6 +120,7 @@ test.describe.serial('test collection', () => {
 				fs.cpSync(testAssetsFolder + '/audio', testCollectionFolder.root + '/.lipupini/audio', {recursive: true})
 				fs.cpSync(testAssetsFolder + '/video', testCollectionFolder.root + '/.lipupini/video', {recursive: true})
 			}
+			await page.waitForTimeout(500) // A little delay to help ensure that the new files are available
 		})
 	}
 
@@ -114,6 +130,7 @@ test.describe.serial('test collection', () => {
 		await expect(page.locator('li a:text-is("' + testCollectionName + '")')).toBeVisible()
 		await page.locator('li a:text-is("' + testCollectionName + '")').click()
 		await page.waitForURL(host + '/@' + testCollectionName)
+		await page.waitForTimeout(2000) // Because we're also testing on-demand static generation which adds some stress
 		let hrefs = []
 		const mediaItemLinks = await page.locator('#folder main.grid a').all()
 		for (const mediaItemLink of mediaItemLinks) {
@@ -121,6 +138,7 @@ test.describe.serial('test collection', () => {
 		}
 		for (const href of hrefs) {
 			await page.goto(host + href)
+			await page.waitForTimeout(750) // Because we're also testing on-demand static generation which adds some stress
 			let mediaType = (await page.locator('#media-item').getAttribute('class')).replace(/-item/, '')
 			switch (mediaType) {
 				case 'image':
@@ -154,7 +172,7 @@ test.describe.serial('test collection', () => {
 	test('check RSS URLs', async ({page, request}) => {
 		const rssUrl = host + '/rss/' + testCollectionName + '/' + testCollectionName + '-feed.rss'
 		await page.goto(host + '/@' + testCollectionName)
-		await expect(await page.locator('link[rel="alternate"][type="application/rss+xml"]')).toHaveAttribute('href', rssUrl)
+		await expect(page.locator('link[rel="alternate"][type="application/rss+xml"]')).toHaveAttribute('href', rssUrl)
 		expect((await request.get(rssUrl)).ok()).toBeTruthy()
 	})
 
@@ -163,7 +181,8 @@ test.describe.serial('test collection', () => {
 		const apiResponse = await request.get(apiUrl)
 		expect(apiResponse.ok()).toBeTruthy()
 		const apiResponseBody = JSON.parse((await apiResponse.body()).toString());
-		expect(Object.keys(apiResponseBody.data).length).toEqual(12)
+		const supportsAvifResult = await supportsAvif(page)
+		expect(Object.keys(apiResponseBody.data).length).toEqual(supportsAvifResult ? 12 : 11)
 		expect((await request.get(apiUrl + '/blank.png.json')).ok()).toBeTruthy()
 	})
 
@@ -184,14 +203,15 @@ test.describe.serial('test collection', () => {
 	test('analyze collection cache files', async () => {
 		const analyzeCacheResult = askPhp('analyzeCache ' + testCollectionName)
 		console.log(analyzeCacheResult)
-		await expect(analyzeCacheResult.messages).toEqual(undefined)
+		expect(analyzeCacheResult.messages).toEqual(undefined)
 	})
 
 	if (testCollectionPagination) {
 		test('test pagination and navigation in header and footer', async ({page}) => {
 			if (createNewCollection) {
+				const supportsAvifResult = await supportsAvif(page)
 				// Add enough files to paginate
-				for (let i = 1; i <= 25; i++) {
+				for (let i = 1; i <= (supportsAvifResult ? 25 : 26); i++) {
 					fs.copyFileSync(testAssetsFolder + '/blank.png', testCollectionFolder.root + '/' + i + '.png')
 				}
 			}
@@ -199,10 +219,10 @@ test.describe.serial('test collection', () => {
 				await page.goto(host + '/@' + testCollectionName)
 				await expect(page.locator(navLocation + ' nav .previous .button')).toHaveAttribute('disabled')
 				await page.locator(navLocation + ' nav .next .button').click()
-				await expect(page).toHaveURL(host + '/@' + testCollectionName + '?page=2')
+				await page.waitForURL(host + '/@' + testCollectionName + '?page=2')
 				await expect(page.locator(navLocation + ' nav .next .button')).toHaveAttribute('disabled')
 				await page.locator(navLocation + ' nav .previous .button').click()
-				await expect(page).toHaveURL(host + '/@' + testCollectionName)
+				await page.waitForURL(host + '/@' + testCollectionName)
 			}
 		})
 	}
